efc73c43a48ec48167db54e7b8f5ab0e1b55d5d4
Keith Waters <keith@readingwaters.com>
2019-09-27 10:33:16 -0400

    Basic hello world up and running

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Index extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>
+          Hello Next.js
+        </p>
+      </div>
+    )
+  }
+};
+
+export default Index;
