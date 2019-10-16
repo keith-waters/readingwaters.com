e11c10fef12c48e0791b7fdb0351c77051d87c2c
Keith Waters <keith@readingwaters.com>
2019-10-15 21:54:59 -0400

    Clean up

--- pages/blog/[name].js
+++ pages/blog/[name].js
@@ -1,11 +1,15 @@
 import { Component } from 'react';
 import { Box, Card, CardMedia, Typography } from '@material-ui/core';
 import { MDXProvider } from '@mdx-js/react';
-import MDXDocument from '../content/test.md';
+import MDXDocument from '../../content/test.md';
 
 const H1 = props => <Typography {...props} component="h1" variant="h3" />
 
 class Blog extends Component {
+  static getInitialProps(query, stuff){
+    console.log('quirey', query)
+    return {};
+  }
 
   render() {
     const components = {
