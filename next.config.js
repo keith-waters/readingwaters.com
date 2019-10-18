9f657e96fd2d2222180637bc0aab9b2f12003cfa
Keith Waters <keith@readingwaters.com>
2019-10-17 21:14:44 -0400

    Simplifying mdx implemendation, it's better for now

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/blog/stat': { page: '/blog', query: { name: 'NAME' } },
-    }
-  },
 })
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,17 +1,19 @@
 import { Component } from 'react';
 import { Box, Card, CardMedia, Typography } from '@material-ui/core';
 
-
-class Index extends Component {
+class Articles extends Component {
 
   render() {
     return (
       <>
         <Card style={{padding:"10px", textAlign:"center"}}>
+            <Typography>
+                Articles
+            </Typography>
         </Card>
       </>
     )
   }
 };
 
-export default Index;
+export default Articles;
