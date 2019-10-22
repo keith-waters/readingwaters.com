c7d186b6ab032d7a3a11f69b2fe57379abc86c24
Keith Waters <keith@readingwaters.com>
2019-10-21 20:29:01 -0400

    Cleaning out all the experimental code

--- next.config.js
+++ next.config.js
@@ -1,39 +1,7 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
-const fs = require('fs');
-const remark = require('remark');
-const frontmatter = require('remark-frontmatter');
-const yaml = require('js-yaml');
-
-const files = fs.readdirSync('./content/');
-const articlesPathMap = {};
-const articlesList = [];
-
-files.forEach(file => {
-  const content = fs.readFileSync(`./content/${file}`, 'utf8');
-  const updateArticlesData = () => data => {
-    // Grabbing the first element in children because I know it will be the
-    // YAML child. A better way is to find the items in the children array 
-    // that have a "type" of "yaml".
-    const metadata = yaml.load(data.children[0].value);
-    articlesPathMap[`/articles/${metadata.slug}`] = { page: '/articles/[slug]', query: {content, metadata}};
-    articlesList.push(metadata);
-  };
-
-  remark()
-    .use(frontmatter, ['yaml'])
-    .use(updateArticlesData)
-    .processSync(content)
-});
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  // exportPathMap: async () => {
-  //   return {
-  //     '/': { page: '/' },
-  //     '/articles': { page: '/articles', query: {data: articlesList} },
-  //     ...articlesPathMap,
-  //   }
-  // },
 });
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react';
 import Link from 'next/link'
 
 class Articles extends Component {
-  static async getInitialProps({query}){
-    return { list: query.data || [] }
-  }
-
   render() {
-    const { list } = this.props;
     return (
       <>
         <p>Articles</p>
-        {
-          list.map(item => {
-            return (
-              <a href={`/articles/${item.slug}`} key={Math.random()}>{item.title}</a>
-            )
-          })
-        }
+        <a href={'/articles/jam'} key={Math.random()}>amoino</a>
       </>
     )
   }
