0c6d44010cf9fa4f2b722ac5559291d0b23a6e06
Keith Waters <keith@readingwaters.com>
2019-10-22 21:19:53 -0400

    Totally remove material-ui!

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,8 @@
-import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-import theme from '../theme';
 import Layout from '../Layout';
 
 class MyApp extends App {
-  componentDidMount() {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }
-
   render() {
     const { Component, pageProps } = this.props;
 
@@ -24,13 +11,9 @@ class MyApp extends App {
         <Head>
           <title>ReadingWaters</title>
         </Head>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </>
     );
   }
