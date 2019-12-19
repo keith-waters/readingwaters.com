e76bca639693d75f64e0915dccb68dd50f19ef9b
Keith Waters <keith@readingwaters.com>
2019-12-19 18:40:35 -0500

    First draft of Coding Challenge: Max Pairwise Product - Add Article footer component

---
+++ src/components/ArticleFooter.js
@@ -0,0 +1,14 @@
+const ArticleFooter = () => {
+  return (
+    <div style={{marginBottom: "100px", padding: "10px", borderRadius: "5px", backgroundColor: "lightgray"}}>
+      <h2 style={{marginTop: "0"}} >
+        See something you like? Want to continue the conversation?
+      </h2>
+      <p style={{marginBottom: "0"}} >
+        Send an email to <a href="mailto:readingwaters@gmail.com">readingwaters@gmail.com</a>
+      </p>
+    </div>
+  );
+}
+
+export default ArticleFooter;
