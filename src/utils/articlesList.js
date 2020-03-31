cac67824a4497bb9e99daea63f21881284e83baf
Keith Waters <readingwaters@gmail.com>
2020-03-31 08:00:29 -0400

    Create template and add metadata to articlesList

--- src/utils/articlesList.js
+++ src/utils/articlesList.js
@@ -7,4 +7,12 @@ module.exports = [
     logoAlt: '',
     tags: ['coding-challenge', 'algorithms'],
   },
+  {
+    title: 'Writing Comments in Javascript and other languages',
+    url: '/writing-comments-in-javascript-and-other-languages',
+    publishDate: '2020-03-31',
+    logoSrc: '',
+    logoAlt: '',
+    tags: ['intro to programming', 'comments', 'javascript', 'ruby', 'python', 'c', 'c++'],
+  }
 ];
