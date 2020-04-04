913d7fe60d514a79a767d8dbca7e91e51019a31b
Keith Waters <readingwaters@gmail.com>
2020-04-03 21:51:51 -0400

    Fix overflowing tag section on single article page

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -12,12 +12,12 @@ const Metadata = ({metadata}) => {
         <div style={{display: "flex", flexDirection: "column"}}>
           <h1 style={{margin: "0 0 0 0"}}>{title}</h1>
           <p style={{margin: "0 0 0 0"}}>{publishDate}</p>
-          <div style={{display: "flex", flexGrow: "1", alignItems: "flex-end"}}>
+          <div style={{display: "flex", flexGrow: "1", alignItems: "flex-end", width: "85%", flexWrap: "wrap"}}>
             {
               tags.map(tag => (
                 <p 
                   key={tag} 
-                  style={{margin: "0 5px 0 0", padding: "3px 7px", borderRadius: "3px", backgroundColor: "rgba(0,0,0,0.1)"}}
+                  style={{margin: "0 5px 5px 0", padding: "3px 7px", borderRadius: "3px", backgroundColor: "rgba(0,0,0,0.1)"}}
                 >
                   {tag}
                 </p>
