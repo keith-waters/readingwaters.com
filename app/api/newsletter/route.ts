f8b61ce65079bc4fb170cf0ab0b153de9f1e377d
keith-waters <readingwaters@gmail.com>
2024-01-10 14:54:22 -0500

    New blog everything.... and i like it

---
+++ app/api/newsletter/route.ts
@@ -0,0 +1,26 @@
+export async function POST(req: Request) {
+  const { email } = await req.json()
+  const data = {
+    email,
+    status: 'active',
+  }
+
+  try {
+    const mailerliteRes = await fetch('https://connect.mailerlite.com/api/subscribers', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        Authorization: `Bearer ${process.env.MAILER_LITE_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    const mlRes = await mailerliteRes.json()
+    if (mlRes.errors) {
+      return Response.json({ error: 'There was an error subscribing' }, { status: 400 })
+    }
+
+    return Response.json({ message: 'success' })
+  } catch (err) {
+    return Response.json({ error: 'There was an error subscribing' }, { status: 500 })
+  }
+}
---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'SmolPage',
+    description: `A link in bio tool for creators on Kick.com`,
+    imgSrc: '/static/images/projects/smolpage-com-screenshot.png',
+    href: 'https://smolpage.com',
+  },
+  {
+    title: 'TypeScript Jobs',
+    description: `A job board for TypeScript job posts`,
+    imgSrc: '/static/images/projects/typescriptjobs-io-screenshot.png',
+    href: 'https://typescriptjobs.io',
+  },
+]
+
+export default projectsData
