aa6764164bfbc423df331c5cf08c9e19b0e5e51c
keith-waters <readingwaters@gmail.com>
2024-02-04 09:06:30 -0500

    orsitn

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Remnant 2 Gear',
+    description: `A gear filter for the Remnant 2 video game.`,
+    imgSrc: '/static/images/projects/remnant2gear-com-screenshot.png',
+    href: 'https://remnant2gear.com',
+  },
+  {
     title: 'SmolPage',
     description: `A link in bio tool for creators on Kick.com`,
     imgSrc: '/static/images/projects/smolpage-com-screenshot.png',
